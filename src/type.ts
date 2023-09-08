d086cb6fb079366cf1572db71a0c520e99fd9587
fmaulll <maul2821@gmail.com>
2023-09-08 16:09:26 +0700

    type for movie rensponse

---
+++ src/type.ts
@@ -0,0 +1,15 @@
+export type MoviesType = {
+  title: string;
+  year: string;
+  imdbID: string;
+  type: string;
+  poster: string;
+  price: number;
+};
+export type MoviesResType = {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+};
