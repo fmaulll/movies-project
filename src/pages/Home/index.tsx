9ac5e34b1158772909a98a9a1a356655412e09e7
fmaulll <maul2821@gmail.com>
2023-09-09 01:30:56 +0700

    changes in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,22 +2,41 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import MovieItem from "../../components/MovieItem";
 import { MoviesResType, MoviesType } from "../../type";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { useAppDispatch } from "../../hooks";
+import { setLoading } from "../../features/movieSlice";
 
 const apiKey = "1082ebc";
 
 function Home() {
+  const dispatch = useAppDispatch();
   const [search, setSearch] = useState<string>("movie");
   const [page, setPage] = useState<number>(1);
   const [movies, setMovies] = useState<MoviesType[]>([]);
+  const [totalRow, setTotalRow] = useState<number>(0);
+
+  const startIndex = (page - 1) * 10;
+  const endIndex = startIndex + 10;
+
+  const handleNext = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const handlePrev = () => {
+    if (page !== 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
 
   const getMovies = async () => {
+    dispatch(setLoading(true));
     try {
       const results = await axios.get(
         `http://www.omdbapi.com/?apikey=${apiKey}&s=${search}&page=${page}`
       );
 
       if (results.status === 200) {
-        const { Search } = results.data;
+        const { Search, totalResults } = results.data;
 
         const arrMovies: MoviesType[] = [];
         Search.map((item: MoviesResType) => {
@@ -28,7 +47,7 @@ function Home() {
               imdbID: item.imdbID,
               type: item.Type,
               poster: item.Poster,
-              price: Math.floor(Math.random() * (1000000 - 100000)) + 100000
+              price: (Math.floor(Math.random() * (1000 - 100)) + 100) * 1000,
             });
           }
         });
@@ -36,20 +55,42 @@ function Home() {
         setMovies((prev) => {
           return [...prev, ...arrMovies];
         });
+        setTotalRow(totalResults);
       }
-    } catch (error) {}
+    } catch (error) {
+      alert(error);
+    } finally {
+      dispatch(setLoading(false));
+    }
   };
 
   useEffect(() => {
     getMovies();
-  }, []);
+  }, [page]);
 
   return (
     <div className="mt-32">
       <div className="grid grid-cols-5 gap-8">
-        {movies.map((item, index) => (
+        {movies.slice(startIndex, endIndex).map((item, index) => (
           <MovieItem key={index} data={item} />
         ))}
+      </div>
+      <div className="flex items-center mt-4">
+        <div
+          onClick={handlePrev}
+          className="px-4 py-2 rounded-xl border-2 mr-4 cursor-pointer"
+        >
+          <FaChevronLeft className="text-white" size={30} />
+        </div>
+        <div
+          onClick={handleNext}
+          className="px-4 py-2 rounded-xl border-2 cursor-pointer"
+        >
+          <FaChevronRight className="text-white" size={30} />
+        </div>
+        <span className="ml-4 text-white">
+          {startIndex + 1} - {endIndex} of {totalRow}
+        </span>
       </div>
     </div>
   );
