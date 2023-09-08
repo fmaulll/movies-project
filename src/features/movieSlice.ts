45115c7e5e2d6e667aa1076d59b475f0ff85826d
fmaulll <maul2821@gmail.com>
2023-09-09 01:30:33 +0700

    added delete and loading state reducer

--- src/features/movieSlice.ts
+++ src/features/movieSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { MoviesType } from "../type";
 // import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface MovieState {
@@ -10,10 +11,12 @@ export interface MovieState {
     poster: string;
     price: number;
   }[];
+  loading: boolean;
 }
 
 const initialState: MovieState = {
   cart: [],
+  loading: false,
 };
 
 export const movieSlice = createSlice({
@@ -21,7 +24,7 @@ export const movieSlice = createSlice({
   initialState,
   reducers: {
     setCart: (state, action) => {
-        const obj = action.payload
+        const obj: MoviesType = action.payload
         const arrCart = state.cart
 
         if (!arrCart.find((item) => item.imdbID === obj.imdbID)) {
@@ -29,11 +32,23 @@ export const movieSlice = createSlice({
         }
 
         state.cart = arrCart
+    },
+    deleteCartItem: (state, action) => {
+      const obj: MoviesType = action.payload
+      const arrCart = state.cart
+      const index = arrCart.findIndex((item) => item.imdbID === obj.imdbID)
+      
+      arrCart.splice(index, 1)
+
+      state.cart = arrCart
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload
     }
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setCart } = movieSlice.actions;
+export const { setCart, deleteCartItem, setLoading } = movieSlice.actions;
 
 export default movieSlice.reducer;
