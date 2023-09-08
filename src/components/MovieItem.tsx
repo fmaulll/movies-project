54014493f174c372c2ea110944f0837945759eff
fmaulll <maul2821@gmail.com>
2023-09-09 01:30:17 +0700

    finish movie item component

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
-import { BsCartPlusFill, BsFillCartCheckFill } from "react-icons/bs";
+import { BsCartPlusFill, BsFillCartCheckFill, BsFillCartXFill } from "react-icons/bs";
 import { useAppDispatch, useAppSelector } from "../hooks";
-import { setCart } from "../features/movieSlice";
+import { deleteCartItem, setCart } from "../features/movieSlice";
 import { MoviesType } from "../type";
+import { convertRupiah } from "../helper";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   data: {
@@ -16,6 +18,7 @@ interface Props {
 }
 
 const MovieItem: FC<Props> = ({ data }) => {
+  const navigate = useNavigate()
   const dispatch = useAppDispatch();
   const cart = useAppSelector((state) => state.movie.cart);
 
@@ -27,6 +30,14 @@ const MovieItem: FC<Props> = ({ data }) => {
     dispatch(setCart(data));
   };
 
+  const handleDeleteCartItem = () => {
+    dispatch(deleteCartItem(data))
+  }
+
+  const handleLink = () => {
+    navigate(`/detail/${data.imdbID}`)
+  }
+
   return (
     <div className="w-full shadow-xl rounded-xl bg-white relative group">
       <img
@@ -34,7 +45,12 @@ const MovieItem: FC<Props> = ({ data }) => {
         src={`http://img.omdbapi.com/?apikey=1082ebc&i=${data.imdbID}`}
         alt={data.title}
       />
-      <div className="flex justify-center items-end w-full h-full absolute bg-black z-10 bottom-0 invisible bg-black-transparent-visible group-hover:visible duration-200 rounded-xl p-4">
+      {!validateCartItem(data) ? null : (
+        <div className="absolute top-0 right-0 p-5 bg-green-600 rounded-full mt-1 mr-1 shadow-xl z-10">
+          <BsFillCartCheckFill className="text-slate-900" size={30} />
+        </div>
+      )}
+      <div className="flex flex-col items-center justify-end w-full h-full absolute bg-black z-10 bottom-0 invisible bg-black-transparent-visible group-hover:visible rounded-xl p-4">
         {!validateCartItem(data) ? (
           <div
             onClick={handleAddToCart}
@@ -43,8 +59,8 @@ const MovieItem: FC<Props> = ({ data }) => {
             <BsCartPlusFill className="text-slate-900" size={30} />
           </div>
         ) : (
-          <div className="absolute top-0 right-0 p-5 bg-green-600 rounded-full mt-1 mr-1 shadow-xl z-10">
-            <BsFillCartCheckFill className="text-slate-900" size={30} />
+          <div onClick={handleDeleteCartItem} className="absolute top-0 right-0 p-5 bg-red-600 rounded-full mt-1 mr-1 shadow-xl z-10 cursor-pointer">
+            <BsFillCartXFill className="text-slate-900" size={30} />
           </div>
         )}
         <div
@@ -53,9 +69,14 @@ const MovieItem: FC<Props> = ({ data }) => {
         >
           <BsCartPlusFill className="text-slate-900" size={30} />
         </div>
-        <h3 className="font-bold text-2xl text-white text-center hover:text-purple-800 cursor-pointer duration-200">
+        <h3 onClick={handleLink} className="font-bold text-2xl text-white text-center hover:text-purple-800 cursor-pointer duration-200">
           {data.title} {`(${data.year})`}
         </h3>
+        <div>
+          <h3 className="mt-4 font-bold text-3xl text-white text-center">
+            {convertRupiah(data.price)}
+          </h3>
+        </div>
       </div>
     </div>
   );
