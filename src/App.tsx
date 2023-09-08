a62e5e0058a92cb8c63876282625ae2a39f90f23
fmaulll <maul2821@gmail.com>
2023-09-09 01:29:36 +0700

    add detail path

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./layout";
+import Detail from "./pages/Detail";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
         </Routes>
       </Layout>
     </BrowserRouter>
