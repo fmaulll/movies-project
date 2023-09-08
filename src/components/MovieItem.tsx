8c426faa742436b7e356b3a2b8d2bab88121cacf
fmaulll <maul2821@gmail.com>
2023-09-08 16:30:58 +0700

    added reducer for cart

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from "react";
-import { BsCartPlusFill } from "react-icons/bs";
+import { BsCartPlusFill, BsFillCartCheckFill } from "react-icons/bs";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { setCart } from "../features/movieSlice";
+import { MoviesType } from "../type";
 
 interface Props {
   data: {
@@ -13,6 +16,17 @@ interface Props {
 }
 
 const MovieItem: FC<Props> = ({ data }) => {
+  const dispatch = useAppDispatch();
+  const cart = useAppSelector((state) => state.movie.cart);
+
+  const validateCartItem = (movie: MoviesType) => {
+    return cart.find((item) => item.imdbID === movie.imdbID);
+  };
+
+  const handleAddToCart = () => {
+    dispatch(setCart(data));
+  };
+
   return (
     <div className="w-full shadow-xl rounded-xl bg-white relative group">
       <img
@@ -21,7 +35,22 @@ const MovieItem: FC<Props> = ({ data }) => {
         alt={data.title}
       />
       <div className="flex justify-center items-end w-full h-full absolute bg-black z-10 bottom-0 invisible bg-black-transparent-visible group-hover:visible duration-200 rounded-xl p-4">
-        <div className="absolute top-0 right-0 p-4 bg-white rounded-full mt-2 mr-2 shadow-xl cursor-pointer">
+        {!validateCartItem(data) ? (
+          <div
+            onClick={handleAddToCart}
+            className="absolute top-0 right-0 p-4 bg-white rounded-full mt-2 mr-2 shadow-xl cursor-pointer"
+          >
+            <BsCartPlusFill className="text-slate-900" size={30} />
+          </div>
+        ) : (
+          <div className="absolute top-0 right-0 p-5 bg-green-600 rounded-full mt-1 mr-1 shadow-xl z-10">
+            <BsFillCartCheckFill className="text-slate-900" size={30} />
+          </div>
+        )}
+        <div
+          onClick={handleAddToCart}
+          className="absolute top-0 right-0 p-4 bg-white rounded-full mt-2 mr-2 shadow-xl cursor-pointer"
+        >
           <BsCartPlusFill className="text-slate-900" size={30} />
         </div>
         <h3 className="font-bold text-2xl text-white text-center hover:text-purple-800 cursor-pointer duration-200">
